4fedac54e9faa6fc51c66fa71c5f64e2ded8c8bb
Igor <klevzhits94@icloud.com>
2024-08-13 19:42:43 +0300

    Create UI on WelcomeViewController

--- FlashChat/WelcomeViewController.swift
+++ FlashChat/WelcomeViewController.swift
@@ -6,10 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
 class WelcomeViewController: UIViewController {
     
     // MARK: - UI
+    
+    private lazy var titleLabel: UILabel = {
+        let element = UILabel()
+        element.textColor = UIColor(named: K.BrandColors.blue)
+        element.font = .systemFont(ofSize: 50, weight: .black)
+        return element
+    }()
+    
+    var registerButton = UIButton(
+        titleColor: UIColor(named: K.BrandColors.blue),
+        backgroundColor: UIColor(named: K.BrandColors.lighBlue)
+    )
+    
+    var logInButton = UIButton(
+        titleColor: .white,
+        backgroundColor: UIColor(named: K.BrandColors.blue)
+    )
     
     // MARK: - Life Cycle
 
@@ -20,10 +38,22 @@ class WelcomeViewController: UIViewController {
         setupConstraints()
     }
     
+    @objc private func buttonsTapped(_ sender: UIButton) {
+        
+    }
+    
     // MARK: - Set Views
     
     private func setViews() {
         view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        view.addSubview(registerButton)
+        view.addSubview(logInButton)
+        
+        titleLabel.text = K.appName
+        registerButton.setTitle(K.registerName, for: .normal)
+        logInButton.setTitle(K.logInName, for: .normal)
     }
 
 }
@@ -33,8 +63,29 @@ class WelcomeViewController: UIViewController {
 extension WelcomeViewController {
     
     private func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
         
+        logInButton.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(K.Size.buttonSize)
+        }
+        
+        registerButton.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(logInButton.snp.top).offset(-K.Size.buttonOffset)
+            make.height.equalTo(K.Size.buttonSize)
+        }
     }
     
 }
 
+extension UIButton {
+    convenience init(titleColor: UIColor?, backgroundColor: UIColor? = .clear) {
+        self.init(type: .system)
+        self.titleLabel?.font = .systemFont(ofSize: 30)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+    }
+}
